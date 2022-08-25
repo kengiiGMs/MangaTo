7bf15dd30071c4d933767857829deaf5c3db9f7b
KengiiGMs <kaikiaoto22@gmail.com>
2022-08-25 10:49:55 -0300

    Estilizado navBar adm

--- Adm/AreaAdm.php
+++ Adm/AreaAdm.php
@@ -7,6 +7,7 @@
     <title>Administração</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styleAdm.css">
 </head>
 
 <body>
@@ -22,16 +23,16 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="cadastroProduto/produto.php">Produto</a>
+                            <a class="nav-link" href="cadastroProduto/produto.php" id="linkMenuAdm">Produto</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="Perfils/perfils.php">Perfils</a>
+                            <a class="nav-link" href="Perfils/perfils.php" id="linkMenuAdm">Perfils</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="Mensagens/mensagens.php">Mensagens</a>
+                            <a class="nav-link" href="Mensagens/mensagens.php" id="linkMenuAdm">Mensagens</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../Site/index.php">Sair</a>
+                            <a class="nav-link" href="../Site/index.php" id="linkMenuAdm">Sair</a>
                         </li>
                 </div>
             </div>
